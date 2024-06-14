2c32bbfad3531bdd9b9057715855083b16c135a0
Rick South <rick@natamus.com>
2024-06-14 05:38:27 +0200

    Configurable Furnace Burn Time source code for 1.21.0.

---
+++ Fabric/src/main/java/com/natamus/configurablefurnaceburntime/ModFabric.java
@@ -0,0 +1,31 @@
+package com.natamus.configurablefurnaceburntime;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectiveFurnaceEvents;
+import com.natamus.configurablefurnaceburntime.events.FurnaceBurnEvent;
+import com.natamus.configurablefurnaceburntime.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.item.ItemStack;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveFurnaceEvents.CALCULATE_FURNACE_BURN_TIME.register((ItemStack itemStack, int burntime) -> {
+			return FurnaceBurnEvent.furnaceBurnTimeEvent(itemStack, burntime);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
