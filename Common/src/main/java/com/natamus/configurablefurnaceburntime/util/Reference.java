ae2d41f87cffb07a4bb4dcdadbd856da4d8fe3ce
Rick South <rick@natamus.com>
2024-07-28 07:39:13 +0200

    (v2.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/configurablefurnaceburntime/util/Reference.java
+++ Common/src/main/java/com/natamus/configurablefurnaceburntime/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.configurablefurnaceburntime.util;
 public class Reference {
 	public static final String MOD_ID = "configurablefurnaceburntime";
 	public static final String NAME = "Configurable Furnace Burn Time";
-	public static final String VERSION = "2.3";
+	public static final String VERSION = "2.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
