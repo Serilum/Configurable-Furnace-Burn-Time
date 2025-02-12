25e789a3a6563e1fe25f81d83189291ad047cf8a
Rick South <rick@natamus.com>
2025-02-12 23:40:30 +0100

    (v2.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/configurablefurnaceburntime/forge/events/ForgeFurnaceBurnEvent.java
+++ Forge/src/main/java/com/natamus/configurablefurnaceburntime/forge/events/ForgeFurnaceBurnEvent.java
@@ -3,12 +3,10 @@ package com.natamus.configurablefurnaceburntime.forge.events;
 import com.natamus.configurablefurnaceburntime.events.FurnaceBurnEvent;
 import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeFurnaceBurnEvent {
 	@SubscribeEvent
-	public void furnaceBurnTimeEvent(FurnaceFuelBurnTimeEvent e) {
+	public static void furnaceBurnTimeEvent(FurnaceFuelBurnTimeEvent e) {
 		int burnTime = e.getBurnTime();
 		int newBurnTime = FurnaceBurnEvent.furnaceBurnTimeEvent(e.getItemStack(), burnTime);
 
